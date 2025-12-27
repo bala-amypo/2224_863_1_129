8d00cb3ae1d3051f319d3ccdb9c16c2093284927
bala-amypo <bala@amypo.in>
2025-12-27 05:15:32 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,31 +1,3 @@
-// package com.example.demo.config;
-
-// import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.info.Info;
-// import io.swagger.v3.oas.models.security.SecurityRequirement;
-// import io.swagger.v3.oas.models.security.SecurityScheme;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-
-// @Configuration
-// public class OpenApiConfig {
-    
-//     @Bean
-//     public OpenAPI customOpenAPI() {
-//         return new OpenAPI()
-//             .info(new Info()
-//                 .title("E-Commerce Bundle API")
-//                 .version("1.0")
-//                 .description("API for managing products, carts, and bundle discounts"))
-//             .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-//             .components(new io.swagger.v3.oas.models.Components()
-//                 .addSecuritySchemes("Bearer Authentication", 
-//                     new SecurityScheme()
-//                         .type(SecurityScheme.Type.HTTP)
-//                         .scheme("bearer")
-//                         .bearerFormat("JWT")));
-//     }
-// }
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
@@ -40,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI openAPI() {
 
         SecurityScheme bearerAuth = new SecurityScheme()
                 .type(SecurityScheme.Type.HTTP)
